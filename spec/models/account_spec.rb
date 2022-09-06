53404bbe878a836c8cf405322e54c044accb7a76
Marco Peixoto <marcoaspeixoto@gmail.com>
2022-09-06 18:13:28 -0300

    test(account): association author and presence account_number

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Account, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should belong_to(:supplier) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:account_number) }
+  end
 end
