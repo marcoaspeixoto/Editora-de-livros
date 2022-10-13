69d3be88e98f4f4e403eb6eef4cee81058717187
Marco Peixoto <marcoaspeixoto@gmail.com>
2022-10-12 21:35:29 -0300

    test(part): association supplier and assemblies

--- spec/models/part_spec.rb
+++ spec/models/part_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Part, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should belong_to(:supplier) }
+  end
+
+  describe 'associations' do
+    it { should have_and_belong_to_many(:assemblies) }
+  end
 end
