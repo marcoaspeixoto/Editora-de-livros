6dd6f39afcc8e9a2fa10effc8df51e26f4542ecf
Marco Peixoto <marcoaspeixoto@gmail.com>
2022-10-17 16:11:36 -0300

    feat(supplier): add column cnpj and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_24_013826) do
+ActiveRecord::Schema.define(version: 2022_10_17_190802) do
 
   create_table "accounts", force: :cascade do |t|
     t.string "account_number"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 2022_09_24_013826) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "cnpj"
   end
 
   add_foreign_key "accounts", "suppliers"
