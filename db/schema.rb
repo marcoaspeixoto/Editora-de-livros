76dbe427401a860b0a0bcc92ea4e79a8481926ea
Marco Peixoto <marcoaspeixoto@gmail.com>
2022-10-18 15:51:50 -0300

    feat(book): add column isbn and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_17_203953) do
+ActiveRecord::Schema.define(version: 2022_10_18_184356) do
 
   create_table "accounts", force: :cascade do |t|
     t.string "account_number"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2022_10_17_203953) do
     t.date "published_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "isbn"
     t.index ["author_id"], name: "index_books_on_author_id"
   end
 
