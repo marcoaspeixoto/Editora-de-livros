69f55c952bce77956dafd39d08829ffc0c3630d8
Marco Peixoto <marcoaspeixoto@gmail.com>
2022-10-18 16:17:10 -0300

    feat(author): cpf permission parameters

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -66,6 +66,6 @@ class AuthorsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def author_params
-      params.require(:author).permit(:name)
+      params.require(:author).permit(:name, :cpf)
     end
 end
