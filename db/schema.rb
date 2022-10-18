272c864d3a82742ae393be8cacfe27206343090f
Marco Peixoto <marcoaspeixoto@gmail.com>
2022-10-18 16:30:25 -0300

    feat(books): add column title and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_18_185558) do
+ActiveRecord::Schema.define(version: 2022_10_18_192125) do
 
   create_table "accounts", force: :cascade do |t|
     t.string "account_number"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 2022_10_18_185558) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "isbn"
+    t.string "title"
     t.index ["author_id"], name: "index_books_on_author_id"
   end
 
