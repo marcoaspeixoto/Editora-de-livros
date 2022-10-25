9c4d1b0bbc537ae9e0215449db45c12f45b415fd
Marco Peixoto <marcoaspeixoto@gmail.com>
2022-10-25 15:06:53 -0300

    feat(supplier): validate cnpj

--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -2,4 +2,11 @@ class Supplier < ApplicationRecord
   has_one :account
   has_many :parts
   validates_presence_of :name, :cnpj
+  validate :validate_cnpj
+
+  private
+
+  def validate_cnpj
+    errors.add(:cnpj, "CNPJ inválido! Por favor, tente novamente.") unless CNPJ.valid?(cnpj)
+  end
 end
